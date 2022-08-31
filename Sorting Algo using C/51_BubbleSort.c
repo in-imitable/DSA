e3a58fe62d2510fe43000067924426a46e523ac1
in-imitable <atul69030@gmail.com>
2022-08-31 13:09:12 +0530

    Modifying bubbleSort to make it adaptive

--- Sorting Algo using C/51_BubbleSort.c
+++ Sorting Algo using C/51_BubbleSort.c
@@ -7,15 +7,10 @@ void display(int * arr, int n){
     printf("\n");
 }
 
-// void swap(int a, int b){
-//     int temp = a;
-//     a = b;
-//     b = temp;
-// }
-
 void bubbleSort(int * arr, int n){
     for (int i = 0; i < n-1; i++)   //For number of passes
-    {
+    {   
+        printf("Working on pass number %d\n", i+1);
         for (int j = 0; j < n-1-i; j++) //For comparison of each passes
         {
             if(arr[j]>arr[j+1]){
@@ -23,17 +18,46 @@ void bubbleSort(int * arr, int n){
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
             }
-        }  
+        }
+    }    
+}
+
+//Modifying bubbleSort to make it adaptive to better performance
+void bubbleSortAdaptive(int * arr, int n){
+    int isSorted = 0;
+    for (int i = 0; i < n-1; i++)   //For number of passes
+    {   
+        printf("Working on pass number %d\n", i+1);
+        isSorted = 1;
+        for (int j = 0; j < n-1-i; j++) //For comparison of each passes
+        {
+            if(arr[j]>arr[j+1]){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                isSorted = 0;
+            }
+        } 
+        if(isSorted){
+            return;
+        }
     }    
 }
 
 int main(){
     
     int arr[] = {21, 65, 68, 74, 24, 15, 62, 23, 88, 26};
+    // int arr[] = {21, 25, 29, 35, 37, 42, 52, 56, 75, 85};
     int n = 10;
 
     display(arr, n);
-    bubbleSort(arr, n);
+
+    // printf("Number of passes is higher\n");
+    // bubbleSort(arr, n);
+    
+    printf("Number of passes is lesser and better performace\n");
+    bubbleSortAdaptive(arr, n);
+    
     display(arr, n);
 
     return 0;
